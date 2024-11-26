3a7a7f18db66517e0414a5772718a6ff7c1bb916
dulesh1234 <153369407+dulesh1234@users.noreply.github.com>
2024-11-26 17:40:17 +0530

    Update index.js

--- chat-app-backend/index.js
+++ chat-app-backend/index.js
@@ -37,7 +37,7 @@ const server = app.listen(process.env.PORT, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: "https://chatcraze.onrender.com",
+    origin: "https://buzzchatty.onrender.com",
     credentials: true,
   },
 });
